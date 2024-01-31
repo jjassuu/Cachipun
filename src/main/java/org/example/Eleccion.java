e69fea48a8eca349b42b261119131f60464b3ed5
jjassuu <118200868+jjassuu@users.noreply.github.com>
2024-01-31 12:17:44 +0100

    Update Eleccion.java

--- src/main/java/org/example/Eleccion.java
+++ src/main/java/org/example/Eleccion.java
@@ -1,6 +1,7 @@
 package org.example;
 import java.util.*;
 
+//Genera la elección aleatoria de la computadora, y muestra en pantalla la elección del jugador y la del ordenador.
 public class Eleccion {
     //Constante que genera numeros aleatorios
     private static final Random random = new Random();
@@ -9,7 +10,7 @@ public class Eleccion {
         return random.nextInt(3) + 1; /*Genera un numero aleatorio entre 0 y 3,
          siendo el tres exclusivo. Despues se le suma 1*/
     }
-    /*Este método se declara para que muestre en pantalla la eleccion del jugador y la eleccion
+    /*Este método se declara para que muestre en pantalla la eleccion del jugador y la eleccion de la 
     computadora*/
     public static void mostrarEleccion(int player, int computer, String textoPlayer,String textoComputer) {
         System.out.println("Has elegido: " + textoPlayer);
